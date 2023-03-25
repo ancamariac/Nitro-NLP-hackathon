8a9e372c6bea6c0887fe1e76ae465afee54205cd
ancamariac <ciuciuancamaria@gmail.com>
2023-03-25 11:50:37 +0200

    Added embeddings generator

--- data/random_seed_setter.py
+++ data/random_seed_setter.py
@@ -1,4 +1,4 @@
-seed = 1000 # TODO: replace this with the random seed of your team!
+seed = 33 # TODO: replace this with the random seed of your team!
 
 # Note that this should be the general syntax and serves as a guidance,
 # errors might appear because of the versions that these packages use
@@ -16,7 +16,7 @@ torch.use_deterministic_algorithms(True)
 
 import tensorflow as tf
 tf.random.set_seed(seed)
-tf.set_random_seed(seed)
+tf.random.set_seed(seed)
 
 import jax
 jax.random.PRNGKey(seed)
